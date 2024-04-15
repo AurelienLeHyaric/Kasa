a06d28f409a41d2634b22d016f20b1e066dab6cc
Aurélien Le Hyaric <aurelien.lehyaric@live.com>
2024-04-15 14:57:12 +0200

    collapse aboutpage

--- src/components/BtnCollapse/BtnCollapse.jsx
+++ src/components/BtnCollapse/BtnCollapse.jsx
@@ -1,11 +1,22 @@
-import React from "react"
+import React, { useState } from "react"
 import "./BtnCollapse.scss"
+import { ReactComponent as ArrowUp } from "../../assets/images/arrow-up.svg"
+import { ReactComponent as ArrowDown } from "../../assets/images/arrow-down.svg"
 
-function BtnCollapse({ title }) {
+function BtnCollapse({ title, id, children }) {
+   const [isOpen, setIsOpen] = useState(false)
+   const switchEtat = () => {
+      setIsOpen(!isOpen)
+   }
+
    return (
-      <button className="btn-collapse">
-         <h3 className="h3-title">{title}</h3>
-      </button>
+      <>
+         <button id={id} className="btn-collapse">
+            <h3 className="h3-title">{title}</h3>
+            {isOpen ? <ArrowDown onClick={switchEtat} /> : <ArrowUp onClick={switchEtat} />}
+         </button>
+         {isOpen && <div className="collapse-conteneur">{children}</div>}
+      </>
    )
 }
 export default BtnCollapse
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,10 +10,22 @@ function AboutPage() {
             <Banner></Banner>
          </div>
          <section className="aboutmenu">
-            <BtnCollapse title="Fiabilité"></BtnCollapse>
-            <BtnCollapse title="Respect"></BtnCollapse>
-            <BtnCollapse title="Service"></BtnCollapse>
-            <BtnCollapse title="Sécurité"></BtnCollapse>
+            <BtnCollapse id="aboutcollapse" title="Fiabilité">
+               <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+            </BtnCollapse>
+            <BtnCollapse id="aboutcollapse" title="Respect">
+               <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </BtnCollapse>
+            <BtnCollapse id="aboutcollapse" title="Service">
+               <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+            </BtnCollapse>
+            <BtnCollapse id="aboutcollapse" title="Sécurité">
+               <p>
+                  La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une
+                  note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité
+                  domestique pour nos hôtes.
+               </p>
+            </BtnCollapse>
          </section>
       </>
    )
